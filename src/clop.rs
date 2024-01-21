beb4114989cad627ecf0a09626d8c57f8ada7078
wick3dr0se <wick3dr0se@protonmail.com>
2024-01-21 14:54:36 -0500

    switch from char to String type

--- src/clop.rs
+++ src/clop.rs
@@ -2,7 +2,7 @@ use std::env;
 
 pub struct Opts {
     pub long: Vec<(String, Option<String>)>,
-    pub short: Vec<(char, Option<String>)>,
+    pub short: Vec<(String, Option<String>)>,
 }
 
 pub fn get_opts() -> Opts {
@@ -26,17 +26,17 @@ pub fn get_opts() -> Opts {
                     }
                 } else if arg.starts_with("-") {
                     let chars: Vec<char> = arg[1..].chars().collect();
-                    let last_char = chars.last().unwrap();
+                    let last_char = chars.last().unwrap().to_string();
                     let value = iter.clone().next().filter(|v| !v.starts_with("-")).cloned();
 
                     for c in &chars[..chars.len() - 1] {
-                        if !options.short.iter().any(|(o, _)| o == c) {
-                            options.short.push((*c, None));
+                        if !options.short.iter().any(|(o, _)| *o == c.to_string()) {
+                            options.short.push((c.to_string(), None));
                         }
                     }
                     
-                    if !options.short.iter().any(|(o, _)| o == last_char) {
-                        options.short.push((*last_char, value));
+                    if !options.short.iter().any(|(o, _)| *o == last_char) {
+                        options.short.push((last_char, value));
                     }
                 }
             }
