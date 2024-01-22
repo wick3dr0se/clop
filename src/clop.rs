aebb419b27c266fa2af71da17c5f08e001943c7f
wick3dr0se <wick3dr0se@protonmail.com>
2024-01-22 11:41:28 -0500

    add leftovers; prevent option arguments from bleeding into leftover arguments

--- src/clop.rs
+++ src/clop.rs
@@ -2,16 +2,18 @@ use std::env;
 
 pub struct Opts {
     pub long: Vec<(String, Option<String>)>,
-    pub short: Vec<(String, Option<String>)>
+    pub short: Vec<(String, Option<String>)>,
+    pub leftover: Vec<String>,
 }
 
 pub fn get_opts() -> Opts {
     let mut options = Opts {
         long: vec![],
-        short: vec![]
+        short: vec![],
+        leftover: vec![],
     };
     let args: Vec<String> = env::args().collect();
-    let mut iter = args.iter().skip(1);
+    let mut iter = args.iter().skip(1).peekable();
 
     while let Some(arg) = iter.next() {
         match arg.as_str() {
@@ -19,25 +21,35 @@ pub fn get_opts() -> Opts {
             _ => {
                 if arg.starts_with("--") {
                     let option = arg[2..].to_string();
-                    let value = iter.clone().next().filter(|v| !v.starts_with("-")).cloned();
+                    let value = iter.peek().filter(|v| !v.starts_with("-")).cloned();
                     
+                    if value.is_some() {
+                        iter.next();
+                    }
+
                     if !options.long.iter().any(|(o, _)| o == &option) {
-                        options.long.push((option, value));
+                        options.long.push((option, value.cloned()));
                     }
                 } else if arg.starts_with("-") {
                     let chars: Vec<char> = arg[1..].chars().collect();
                     let last_char = chars.last().unwrap().to_string();
-                    let value = iter.clone().next().filter(|v| !v.starts_with("-")).cloned();
+                    let value = iter.peek().filter(|v| !v.starts_with("-")).cloned();
+
+                    if value.is_some() {
+                        iter.next();
+                    }
 
                     for c in &chars[..chars.len() - 1] {
                         if !options.short.iter().any(|(o, _)| *o == c.to_string()) {
                             options.short.push((c.to_string(), None));
                         }
                     }
-                    
+
                     if !options.short.iter().any(|(o, _)| *o == last_char) {
-                        options.short.push((last_char, value));
+                        options.short.push((last_char, value.cloned()));
                     }
+                } else {
+                    options.leftover.push(arg.to_string());
                 }
             }
         }
@@ -59,6 +71,6 @@ impl Opts {
                 }
             }
         }
-        return false;
+        false
     }
 }
